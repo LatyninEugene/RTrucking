a0ef333c3d620f4cd7fa3dac65537a69a406ecc0
hitmanet <nathan200033@gmail.com>
2017-11-10 19:35:33 +0300

    Connect firebase

--- app/src/main/java/com/latynin/rtrucking/MainActivity.java
+++ app/src/main/java/com/latynin/rtrucking/MainActivity.java
@@ -3,11 +3,18 @@ package com.latynin.rtrucking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 }
